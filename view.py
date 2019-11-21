4d286589342dae858fa30066cbd322ffe9bf430f
alexis-bec <56959487+alexis-bec@users.noreply.github.com>
2019-11-21 14:08:36 +0100

    Update view.py

--- view.py
+++ view.py
@@ -13,7 +13,6 @@ from controler import Controler
 
 
 
-
 # =============================================================================
 # Fenetres Principal
 
@@ -81,8 +80,29 @@ class ActionWidget(QWidget):
         layoutV.setAlignment(self.button_changercompte,Qt.AlignBottom)
         self.setLayout(layoutV)
         # connecter les boutons
+        self.button_ajouterdepense.clicked.connect(self.ouvrir_ajouter_depense)
+        self.button_ajoutergain.clicked.connect(self.ouvrir_ajouter_gain)
+        self.button_virementauto.clicked.connect(self.ouvrir_virement_auto)
         self.button_changercompte.clicked.connect(self.ouvrir_select_compte)
         self.button_supprimercompte.clicked.connect(self.ouvrir_supprime_compte)
+        
+    def ouvrir_ajouter_depense(self):
+        self.fenetre = AjouterDepense(None,self.controler)
+        self.fenetre.setWindowModality(Qt.ApplicationModal)
+        self.fenetre.setWindowFlags(Qt.FramelessWindowHint)
+        self.fenetre.show()
+        
+    def ouvrir_ajouter_gain(self):
+        self.fenetre = AjouterGain(None,self.controler)
+        self.fenetre.setWindowModality(Qt.ApplicationModal)
+        self.fenetre.setWindowFlags(Qt.FramelessWindowHint)
+        self.fenetre.show()
+        
+    def ouvrir_virement_auto(self):
+        self.fenetre = VirementAutomatique(None,self.controler)
+        self.fenetre.setWindowModality(Qt.ApplicationModal)
+        #self.fenetre.setWindowFlags(Qt.FramelessWindowHint)
+        self.fenetre.show()
         
     def ouvrir_select_compte(self):
         self.fenetre = SelectCompte(None,self.controler)
@@ -211,6 +231,7 @@ class SelectCompte(QWidget):
     def click_retour(self):
         self.setFixedSize(300,150)
         self.layoutS.setCurrentIndex(0)
+        self.label_erreur.setText("")
 
 
 class SupprimeCompte(QWidget):
@@ -246,7 +267,218 @@ class SupprimeCompte(QWidget):
     def click_non(self):
         self.close()
         
+class AjouterDepense(QWidget):
+    def __init__(self,parent,controler):
+        super().__init__(parent)
+        self.controler = controler
+        self.setFixedSize(500,200)
+        # widgets 
+        self.label_titre = QLabel("<b>Ajouter une dépense</b>")
+        self.label_montant = QLabel("Montant:")
+        self.label_date = QLabel("Date:")
+        self.label_entreprise = QLabel("Bénéficiaire:")
+        self.label_categorie = QLabel("Catégorie:")
+        self.label_commentaire = QLabel("Commentaire:")
+        self.label_erreur = QLabel("")
+        self.text_montant = QLineEdit()
+        self.text_montant.setValidator(QDoubleValidator())
+        self.text_date = QDateEdit(QDate.currentDate())
+        self.text_entreprise = QLineEdit()
+        self.text_categorie = QComboBox()
+        self.text_categorie.addItems(self.controler.get_categorie_list())
+        self.text_commentaire = QLineEdit()
+        self.button = QPushButton("Ajouter")
+        self.button.setMinimumWidth(200)
+        self.button_annuler = QPushButton("Annuler")
+        self.button_annuler.setStyleSheet('font: bold')
+        # layouts
+        layoutG = QGridLayout()
+        layoutG.addWidget(self.label_titre,0,0,1,4)
+        layoutG.setAlignment(self.label_titre,Qt.AlignCenter)
+        layoutG.addWidget(self.label_montant,1,0)
+        layoutG.setAlignment(self.label_montant,Qt.AlignRight)
+        layoutG.addWidget(self.text_montant,1,1)
+        layoutG.addWidget(self.label_date,1,2)
+        layoutG.setAlignment(self.label_date,Qt.AlignRight)
+        layoutG.addWidget(self.text_date,1,3)
+        layoutG.addWidget(self.label_entreprise,2,0)
+        layoutG.setAlignment(self.label_entreprise,Qt.AlignRight)
+        layoutG.addWidget(self.text_entreprise,2,1)
+        layoutG.addWidget(self.label_categorie,2,2)
+        layoutG.setAlignment(self.label_categorie,Qt.AlignRight)
+        layoutG.addWidget(self.text_categorie,2,3)
+        layoutG.addWidget(self.label_commentaire,3,0)
+        layoutG.addWidget(self.text_commentaire,3,1,1,3)
+        layoutG.addWidget(self.label_erreur,4,0)
+        layoutG.addWidget(self.button,4,0,1,4)
+        layoutG.setAlignment(self.button,Qt.AlignCenter)
+        layoutG.addWidget(self.button_annuler,4,3)
+        layoutG.setAlignment(self.button_annuler,Qt.AlignRight)
+        self.setLayout(layoutG)
+        # connecter les boutons
+        self.button.clicked.connect(self.click)
+        self.button_annuler.clicked.connect(self.click_annuler)
+        
+    def click(self):
+        if self.controler.ajouter_somme_compte("-" + self.text_montant.text(),
+                                               self.text_entreprise.text(),
+                                               self.text_date.date(),
+                                               self.text_categorie.currentIndex(),
+                                               self.text_commentaire.text()):
+            self.close()
+        else:
+            self.label_erreur.setText(
+                    "<b><p style='color:red;'>Erreur")
+    
+    def click_annuler(self):
+        self.close()
+        
 
+class AjouterGain(QWidget):
+    def __init__(self,parent,controler):
+        super().__init__(parent)
+        self.controler = controler
+        self.setFixedSize(500,200)
+        # widgets 
+        self.label_titre = QLabel("<b>Ajouter un gain</b>")
+        self.label_montant = QLabel("Montant:")
+        self.label_date = QLabel("Date:")
+        self.label_entreprise = QLabel("Emetteur:")
+        self.label_categorie = QLabel("Catégorie:")
+        self.label_commentaire = QLabel("Commentaire:")
+        self.label_erreur = QLabel("")
+        self.text_montant = QLineEdit()
+        self.text_montant.setValidator(QDoubleValidator())
+        self.text_date = QDateEdit(QDate.currentDate())
+        self.text_entreprise = QLineEdit()
+        self.text_categorie = QComboBox()
+        self.text_categorie.addItems(self.controler.get_categorie_list())
+        self.text_commentaire = QLineEdit()
+        self.button = QPushButton("Ajouter")
+        self.button.setMinimumWidth(200)
+        self.button_annuler = QPushButton("Annuler")
+        self.button_annuler.setStyleSheet('font: bold')
+        # layouts
+        layoutG = QGridLayout()
+        layoutG.addWidget(self.label_titre,0,0,1,4)
+        layoutG.setAlignment(self.label_titre,Qt.AlignCenter)
+        layoutG.addWidget(self.label_montant,1,0)
+        layoutG.setAlignment(self.label_montant,Qt.AlignRight)
+        layoutG.addWidget(self.text_montant,1,1)
+        layoutG.addWidget(self.label_date,1,2)
+        layoutG.setAlignment(self.label_date,Qt.AlignRight)
+        layoutG.addWidget(self.text_date,1,3)
+        layoutG.addWidget(self.label_entreprise,2,0)
+        layoutG.setAlignment(self.label_entreprise,Qt.AlignRight)
+        layoutG.addWidget(self.text_entreprise,2,1)
+        layoutG.addWidget(self.label_categorie,2,2)
+        layoutG.setAlignment(self.label_categorie,Qt.AlignRight)
+        layoutG.addWidget(self.text_categorie,2,3)
+        layoutG.addWidget(self.label_commentaire,3,0)
+        layoutG.addWidget(self.text_commentaire,3,1,1,3)
+        layoutG.addWidget(self.label_erreur,4,0)
+        layoutG.addWidget(self.button,4,0,1,4)
+        layoutG.setAlignment(self.button,Qt.AlignCenter)
+        layoutG.addWidget(self.button_annuler,4,3)
+        layoutG.setAlignment(self.button_annuler,Qt.AlignRight)
+        self.setLayout(layoutG)
+        # connecter les boutons
+        self.button.clicked.connect(self.click)
+        self.button_annuler.clicked.connect(self.click_annuler)
+        
+    def click(self):
+        if self.controler.ajouter_somme_compte(self.text_montant.text(),
+                                               self.text_entreprise.text(),
+                                               self.text_date.date(),
+                                               self.text_categorie.currentIndex(),
+                                               self.text_commentaire.text()):
+            self.close()
+        else:
+            self.label_erreur.setText(
+                    "<b><p style='color:red;'>Erreur")
+    
+    def click_annuler(self):
+        self.close()
+        
+
+class VirementAutomatique(QWidget):
+    def __init__(self,parent,controler):
+        super().__init__(parent)
+        self.controler = controler
+        self.setFixedSize(600,250)
+        # widgets
+        self.label_titre = QLabel("<b>Automatiser entrée/sortie</b>")
+        self.label_erreur = QLabel("")
+        self.label_somme = QLabel("Somme:")
+        self.label_categorie = QLabel("Catégorie:")
+        self.label_entreprise = QLabel("Bénef/Emet:")
+        self.label_commentaire = QLabel("Commentaire:")
+        self.label_debut = QLabel("Début:")
+        self.label_fin = QLabel("Fin:")
+        self.label_echeance = QLabel("Echéance:")
+        self.label_echM = QLabel("M:")
+        self.label_echJ = QLabel("J:")
+        self.label_echH = QLabel("H:")
+        self.text_somme = QLineEdit()
+        self.text_somme.setValidator(QDoubleValidator())
+        self.text_categorie = QComboBox()
+        self.text_categorie.addItems(self.controler.get_categorie_list())
+        self.text_entreprise = QLineEdit()
+        self.text_commentaire = QLineEdit()
+        self.text_debut = QDateTimeEdit(QDate.currentDate())
+        self.text_fin = QDateTimeEdit(QDate.currentDate())
+        self.text_echM = QSpinBox()
+        self.text_echJ = QSpinBox()
+        self.text_echH = QSpinBox()
+        self.button_valider = QPushButton("Valider")
+        self.button_valider.setMinimumWidth(200)
+        self.button_annuler = QPushButton("Annuler")
+        self.button_annuler.setMinimumWidth(100)
+        self.button_annuler.setStyleSheet('font: bold')
+        # layouts
+        layoutH = QHBoxLayout()
+        layoutH.addWidget(self.label_echM)
+        layoutH.setAlignment(self.label_echM,Qt.AlignRight)
+        layoutH.addWidget(self.text_echM)
+        layoutH.addWidget(self.label_echJ)
+        layoutH.setAlignment(self.label_echJ,Qt.AlignRight)
+        layoutH.addWidget(self.text_echJ)
+        layoutH.addWidget(self.label_echH)
+        layoutH.setAlignment(self.label_echH,Qt.AlignRight)
+        layoutH.addWidget(self.text_echH)
+        layoutG = QGridLayout()
+        layoutG.addWidget(self.label_titre,0,0,1,4)
+        layoutG.setAlignment(self.label_titre,Qt.AlignCenter)
+        layoutG.addWidget(self.label_somme,1,0)
+        layoutG.setAlignment(self.label_somme,Qt.AlignRight)
+        layoutG.addWidget(self.text_somme,1,1)
+        layoutG.addWidget(self.label_debut,1,2)
+        layoutG.setAlignment(self.label_debut,Qt.AlignRight)
+        layoutG.addWidget(self.text_debut,1,3)
+        layoutG.setAlignment(self.text_debut,Qt.AlignCenter)
+        layoutG.addWidget(self.label_categorie,2,0)
+        layoutG.setAlignment(self.label_categorie,Qt.AlignRight)
+        layoutG.addWidget(self.text_categorie,2,1)
+        layoutG.addWidget(self.label_echeance,2,2)
+        layoutG.setAlignment(self.label_echeance,Qt.AlignRight)
+        layoutG.addLayout(layoutH,2,3)
+        layoutG.addWidget(self.label_entreprise,3,0)
+        layoutG.setAlignment(self.label_entreprise,Qt.AlignRight)
+        layoutG.addWidget(self.text_entreprise,3,1)
+        layoutG.addWidget(self.label_fin,3,2)
+        layoutG.setAlignment(self.label_fin,Qt.AlignRight)
+        layoutG.addWidget(self.text_fin,3,3)
+        layoutG.setAlignment(self.text_fin,Qt.AlignCenter)
+        layoutG.addWidget(self.label_commentaire,4,0)
+        layoutG.setAlignment(self.label_commentaire,Qt.AlignRight)
+        layoutG.addWidget(self.text_commentaire,4,1,1,3)
+        layoutG.addWidget(self.label_erreur,5,0)
+        layoutG.addWidget(self.button_valider,5,0,1,4)
+        layoutG.setAlignment(self.button_valider,Qt.AlignCenter)
+        layoutG.addWidget(self.button_annuler,5,3)
+        self.setLayout(layoutG)
+        
+    
 # =============================================================================
 # Widget contenant toutes les informations du compte selectionné
 class InformationWidget(QWidget):
